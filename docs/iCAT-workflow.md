44b93bdf2141a031d6efd6d4bc8e98a48e9ca3f7
Ryan Lane <r.i.lane@tudelft.nl>
2018-07-12 18:47:22 +0200

    switching to notebook documentation

--- docs/iCAT-workflow.md
+++ docs/iCAT-workflow.md
@@ -1,12 +1,19 @@
 # iCAT Workflow
-A walkthrough for getting started with the iCAT post-processing workflow on the sonic server. This walkthrough assumes that both `render-ws` and `CATMAID` are up and running successfully and that you have gone through the [`iCAT-startup`](https://github.com/lanery/iCAT-workflow/blob/master/docs/iCAT-startup.md) guide (or that someone else has on your behalf).
+A walkthrough for getting started with the iCAT post-processing workflow on the sonic server. This walkthrough assumes that both `render-ws` and CATMAID are up and running successfully and that you have gone through the [`iCAT-startup`](https://github.com/lanery/iCAT-workflow/blob/master/docs/iCAT-startup.md) guide (or that someone else has on your behalf).
 
-First, check that both `render-ws` and `CATMAID` are running.
+First, check that both `render-ws` and CATMAID are running.
 ```
 systemctl status render
 systemctl status catmaid
 ```
-Green light = good.
+And if you get the green light, go to the homepages for `render-ws` and CATMAID
+
+| Service           | Homepage                                      |
+| ----------------- | --------------------------------------------- |
+| `render-ws` views | http://sonic:8080/render-ws/view/index.html   |
+| CATMAID           | http://sonic/catmaid/                         |
+
+Also make sure that the `icat` conda environment is the active environment.
 
 
 ## Clone this repository
@@ -15,17 +22,50 @@ This repository contains sample data and scripts helpful for running through an 
 git clone https://github.com/lanery/iCAT-workflow.git
 ```
 
+#### Sample Data
+The sample data is organized such that each "stack" of images is stored in its own directory. A "stack" is a key concept within the `render-ws` + CATMAID ecosystem that will be expanded upon later. But basically, a stack can be thought of a collection of **one or more** *images* at **one or more** *layers in z* from **one** *imaging channel*. Hence, all of the small EM tiles will belong to one stack as will the large EM tiles, as will each fluorescence channel. It is perhaps easiest to grasp this organization scheme by looking at the directory tree of the sample data (`iCAT-workflow/iCAT_sample_data`)
+```
+iCAT-workflow
+└───┬ iCAT_sample_data
+    ├───┬ amylase
+    │   └─── amylase-00000x00000.ome.tif
+    ├───┬ big_EM
+    │   └──── big_EM-00000x00000.ome.tif
+    ├───┬ hoechst
+    │   └──── hoechst-00000x00000.ome.tif
+    ├───┬ insulin
+    │   └──── insulin-00000x00000.ome.tif
+    └───┬ lil_EM
+        ├──── lil_EM-00008x00011.ome.tif
+        ├──── lil_EM-00008x00012.ome.tif
+        ├──── ...
+        ├──── lil_EM-00012x00015.ome.tif
+        └──── lil_EM-00012x00016.ome.tif
+
+```
+This is (at least for now) the optimal organization scheme for working with image data in the workflow. Unfortunately, it is not how raw data is output by Odemis. How to go from raw Odemis data to nicely organized, iCAT-friendly data will be covered later.
+
+It is assumed that if you are going to be viewing your data in CATMAID, then it is worthy of long term storage. Even though this is just sample data, we will treat it as if it were a real project. Thus, copy the sample data to your long term storage folder.
+```
+cd ./iCAT-workflow/
+cp -a ./iCAT_sample_data/ /long_term_storage/<user>/<data storage folder>/
+```
+
 
 ## First render-python project
-In short, the fundamental entity in `render-ws` is a "stack". A project can have multiple stacks and each stack can contain multiple z layers with each layer containing multiple image tiles. The organizational structure of each stack, its layers, and its layers' tiles are all stored as metadata in a database accessed via http requests. Or something like that. We will now run a `render-python` script to get a feeling for what working with `render-ws` is like.
+A project can have multiple stacks and each stack can contain multiple z layers with each layer containing multiple image tiles. The organizational structure of each stack, its layers, and its layers' tiles are all stored as metadata in a database accessed via http requests. Or something like that. We will now run a `render-python` script to get a feeling for what working with `render-ws` is like.
 
 #### 1 Create Stacks
-In `./iCAT-workflow/render-python_scripts/` open `render_iCAT_demo.py` in a text editor. You will see a bunch of parameter information at the top and then a bunch of `render-python` code commented out. Assuming you are in the `iCAT-workflow` directory, try running the `render_iCAT_demo.py` script
+In `./iCAT-workflow/render-python_scripts/` open `render_iCAT_demo.py` in a text editor. You will see a bunch of parameter information at the top and then a bunch of `render-python` code commented out. On the line
+```python
+'owner': '<owner>'
+```
+change `<owner>` to your username. Then, assuming you are in the `iCAT-workflow` directory, try running the script.
 ```
 python ./render-python_scripts/render_iCAT_demo.py
 ```
 
-If you refresh the `render-ws` dashboard you should see a table appear with the names of the stacks in the script all in the `LOADING` state. You can view information about each stack by clicking on `View --> Metadata` (or by navigating to e.g. http://localhost:8080/render-ws/v1/owner/lanery/project/iCAT_demo/stack/insulin).
+Now, on the `render-ws` view page, go to the `Render Project Dashboard`. You should see a table with the names of the stacks in the script all in the `LOADING` state. You can view information about each stack by clicking on `View --> Metadata` (or by going to `http://sonic:8080/render-ws/v1/owner/<user>/project/iCAT_demo/stack/<stack>`).
 
 #### 2 Import Image Tile Data
 
--- render-python_scripts/render_iCAT_demo.py
+++ render-python_scripts/render_iCAT_demo.py
@@ -12,32 +12,77 @@ from renderapi.transform import AffineModel
 from renderapi.tilespec import TileSpec
 
 
+# ------------
+# 0 Stack Data
+# ------------
+stack_data = {
+    'data_dir': Path('/long_term_storage/rlane/SECOM/iCAT_sample_data'),
+    'tile_convention': '{stack}/{stack}-{c}x{r}.ome.tif',
+    'width': 2048,
+    'height': 2048,
+    'N_sections': 1,
+    'lil_EM': {
+        'px_size': 4.829,  # nm/px
+        'overlap': 20,  # %
+        'intensity_range': (31800, 35200),
+        'scopeId': 'Verios',
+        'cameraId': 'TLD',
+    },
+    'big_EM': {
+        'stacks': ['big_EM'],
+        'px_size': 84.9,  # nm/px
+        'overlap': 20,  # %
+        'intensity_range': (30400, 32100),
+        'scopeId': 'Verios',
+        'cameraId': 'TLD',
+    },
+    'hoechst': {
+        'px_size': 99.6,  # nm/px
+        'overlap': 20,  # %
+        'intensity_range': (2500, 15400),
+        'scopeId': 'SECOM',
+        'cameraId': 'Andor',
+    },
+    'amylase': {
+        'px_size': 99.6,  # nm/px
+        'overlap': 20,  # %
+        'intensity_range': (2000, 8000),
+        'scopeId': 'SECOM',
+        'cameraId': 'Andor',
+    },
+    'insulin': {
+        'px_size': 99.6,  # nm/px
+        'overlap': 20,  # %
+        'intensity_range': (1200, 5000),
+        'scopeId': 'SECOM',
+        'cameraId': 'Andor',
+    }
+}
+
+
 # ---------------
 # 1 Create Stacks
 # ---------------
 
-# Project, stack, and match collection names
-owner = 'lanery'
-project = 'iCAT_demo'
+# Create a renderapi.connect.Render object
+render_connect_params = {
+    'host': 'sonic',
+    'port': 8080,
+    'owner': 'rlane',  # reset to <owner>
+    'project': 'iCAT_demo',
+    'client_scripts': \
+        '/home/catmaid/render/render-ws-java-client/src/main/scripts',
+    'memGB': '2G'
+}
+render = renderapi.connect(**render_connect_params)
+
+# Create (empty) stacks
 stacks = ['lil_EM',
           'big_EM',
           'hoechst',
           'amylase',
           'insulin']
 
-# Create a renderapi.connect.Render object
-render_connect_params = {
-    'host': 'localhost',
-    'port': 8080,
-    'owner': f'{owner}',
-    'project': f'{project}',
-    'client_scripts': \
-        '/usr/local/render/render-ws-java-client/src/main/scripts',
-    'memGB': '2G'
-}
-render = renderapi.connect(**render_connect_params)
-
-# Actually create (empty) stacks
 for stack in stacks:
     renderapi.stack.create_stack(stack, render=render)
 
@@ -46,104 +91,72 @@ for stack in stacks:
 # 2 Import Image Tile Data
 # ------------------------
 
-# Global tile parameters
-data_dir = Path('/data/projects/iCAT_demo/iCAT_sample_data')
-tile_convention = '{stack}-{c}x{r}.ome.tif'
-width = 2048  # px
-height = 2048  # px
-z = 0  # first (and only) section
+def gen_tile_specs(stack, stack_data):
+    """
+    """
+    # Get input from stack_data
+    data_dir = stack_data['data_dir']
+    N_sections = stack_data.get('N_sections', 1)
+    width = stack_data['width']
+    height = stack_data['height']
+    overlap = stack_data.get('overlap', 20)
+    px_size = stack_data[stack]['px_size']
+    intensity_range = stack_data[stack].get('intensity_range', (0, 65535))
 
-#   2a lil EM tiles
-#   ---------------
-# Tile parameters
-res = 4.88  # nm/px
-overlap = 20  # %
-# Acquisition parameters
-intensity_range = (31800, 35200)
-scopeId = 'Verios'
-cameraId = 'TLD'
+    # TODO: use tile_convention instead
+    tiles = list(data_dir.glob(f'{stack}/*.ome.tif'))
+    tile_specs = []
+    for z in range(N_sections):
+        for tile in tiles:
 
+            # TODO: use tile_convention instead
+            c, r = [int(i) for i in re.findall('\d+', tile.name)]
+            x_pos = c * width * (1 - overlap) * px_size
+            y_pos = r * height * (1 - overlap) * px_size
 
-#   2b big EM tiles
-#   ---------------
-# Tile parameters
-res = 84.9  # nm/px
-overlap = 20  # %
-# Acquisition parameters
-intensity_range = (30500, 32100)
-scopeId = 'Verios'
-cameraId = 'TLD'
-
-
-
-#   2c FM tiles
-#   -----------
-# Tile parameters
-res = 99.6  # nm/px
-overlap = 20  # %
-# Acquisition parameters
-intensity_range = (2000, 8000)
-scopeId = 'SECOM'
-cameraId = 'Andor'
-
-
-
-
-
-
-
-
-
-
-
-
-
-for i, stack in enumerate(stacks):
-    tilespecs = []
-    for r in range(N_rows):
-        for c in range(N_cols):
-            x_pos = c * W * (1 - overlaps[i]) * resolutions[i]
-            y_pos = r * H * (1 - overlaps[i]) * resolutions[i]
-            layout = Layout(sectionId=f'{section:05d}',
-                            scopeId='Verios',
-                            cameraId='Andor',
-                            imageRow=0,
-                            imageCol=0,
+            layout = Layout(sectionId=f'{z:05d}',
+                            scopeId=stack_data.get('scopeId'),
+                            cameraId=stack_data.get('cameraId'),
+                            imageRow=r,
+                            imageCol=c,
                             stageX=x_pos,
                             stageY=y_pos,
                             rotation=0.0,
-                            pixelsize=resolutions[i])
+                            pixelsize=px_size)
 
-            # Define a simple transformation - translation based on layout
             at = AffineModel(B0=layout.stageX/layout.pixelsize,
                              B1=layout.stageY/layout.pixelsize)
 
-            # Define unique tile specifications
-            tileId = tile_convention.replace('__c__', f'{c:05d}').replace(
-                                             '__r__', f'{r:05d}')
-            tileId = tileId.replace('tile', stack)
-            tilePath = Path(tile_dir).joinpath(stack).joinpath(
-                            tileId).with_suffix(tile_ext)
-            imageUrl = tilePath.as_posix()
-            ts = TileSpec(tileId=tileId,
-                          z=section,
-                          width=W,
-                          height=H,
-                          minint=intensity_ranges[i][0],
-                          maxint=intensity_ranges[i][1],
-                          imageUrl=imageUrl,
-                          maskUrl=None,
-                          layout=layout,
-                          tforms=[at])
-            # Append each tilespec to list
-            tilespecs.append(ts)
+            tileId = tile.name.split('.')[0]
+            imageUrl = tile.as_uri()
+            tile_spec = TileSpec(tileId=tileId,
+                                 z=z,
+                                 width=width,
+                                 height=height,
+                                 minint=intensity_range[0],
+                                 maxint=intensity_range[1],
+                                 imageUrl=imageUrl,
+                                 maskUrl=None,
+                                 layout=layout,
+                                 tforms=[at])
 
+            tile_specs.append(tile_spec)
+
+    return tile_specs
+
+
+# Generate TileSpecs
+tile_specs = {}
+for stack in stacks:
+
+    # Get TileSpecs from stack_data
+    tile_specs[stack] = gen_tile_specs(stack, stack_data)
+
+    # Import TileSpecs to render
     renderapi.client.import_tilespecs(stack,
-                                      tilespecs,
+                                      tile_specs[stack],
                                       close_stack=True,
                                       render=render)
 
-# Set stack state to complete
-for stack in stacks:
+    # Set stack state to complete
     renderapi.stack.set_stack_state(stack, 'COMPLETE', render=render)
-

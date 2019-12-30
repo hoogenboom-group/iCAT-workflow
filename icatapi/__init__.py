f821b1e71bec118d95da0ab385dfa3e6804d5984
lanery <ryanilane@gmail.com>
2019-12-30 10:49:03 +0100

    Initial iCAT-workflow api
    Former-commit-id: 8fed8d4310a811dc66dd62d88363b6c93a78c26d

---
+++ icatapi/__init__.py
@@ -0,0 +1,5 @@
+from .importo import *
+from .montage import *
+from .exporto import *
+
+__version__ = '0.1.dev'
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+DISTNAME = "iCAT-workflow"
+DESCRIPTION = "Post-processing workflow for CLEM image data"
+MAINTAINER = "Ryan Lane"
+MAINTAINER_EMAIL = "r.i.lane@tudelft.nl"
+LICENSE = "LICENSE"
+URL = "https://github.com/lanery/iCAT-workflow"
+VERSION = "0.1.dev"
+PACKAGES = [
+    "icatapi",
+]
+INSTALL_REQUIRES = [
+    "numpy",
+    "scipy",
+    "scikit-image",
+]
+
+if __name__ == '__main__':
+
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        packages=PACKAGES,
+        include_package_data=True,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=open("README.md").read(),
+        install_requires=INSTALL_REQUIRES,
+    )

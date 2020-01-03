3af5b45922f38997dec0c6b0aeb6912f15a8e3e2
lanery <ryanilane@gmail.com>
2020-01-03 14:35:02 +0100

    Update __init__.py
    Former-commit-id: b600f2f4ecd642d07351ca1c4b9b2e94c4c7c0cc

--- icatapi/__init__.py
+++ icatapi/__init__.py
@@ -1,5 +1,10 @@
 from .importo import *
 from .montage import *
+from .align import *
+from .overlay import *
+from .correlate import *
 from .exporto import *
+
+from render_pandas import *
 
 __version__ = '0.1.dev'

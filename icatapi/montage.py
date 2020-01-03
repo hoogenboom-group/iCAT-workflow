9236bf997c2a12a2e779675529f3fc76a9b385ba
lanery <ryanilane@gmail.com>
2020-01-03 10:34:41 +0100

    Update montage.py
    Former-commit-id: 36e3a46f975311156d1a61940310aa3dd2b525a9

--- icatapi/montage.py
+++ icatapi/montage.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 from renderapi.client import tilePairClient
+from renderapi.stack import get_z_values_for_stack
 
 
 def get_tile_pairs(stack, render):
@@ -24,15 +25,15 @@ def get_tile_pairs(stack, render):
     df_pairs = pd.DataFrame(columns=pairs_cols)
 
     # Iterate through stack's z values
-    z_values = renderapi.stack.get_z_values_for_stack(stack=stack,
-                                                      render=render)
+    z_values = get_z_values_for_stack(stack=stack,
+                                      render=render)
     for z in z_values:
 
         # Generate tile pairs
-        tile_pairs_json = renderapi.client.tilePairClient(stack,
-                                                          minz=z,
-                                                          maxz=z,
-                                                          render=render)
+        tile_pairs_json = tilePairClient(stack,
+                                         minz=z,
+                                         maxz=z,
+                                         render=render)
         # Create DataFrame from json
         df = pd.io.json.json_normalize(tile_pairs_json['neighborPairs'])
         df['z'] = z

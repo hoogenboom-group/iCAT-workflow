ae4897b025f6ef0606642a4f8da99121e64f4245
lanery <ryanilane@gmail.com>
2020-01-06 10:15:52 +0100

    Update montage.py
    Former-commit-id: 30cd985ac0f4e5ecc9d49d3be6922c24216f51c2

--- icatapi/montage.py
+++ icatapi/montage.py
@@ -1,8 +1,12 @@
+import re
+
 import numpy as np
 import pandas as pd
+from pd.io.json import json_normalize
 
 from renderapi.client import tilePairClient
 from renderapi.stack import get_z_values_for_stack
+from rendearpi.pointmatch import get_matches_within_group
 
 
 __all__ = ['get_tile_pairs_4_montage']
@@ -44,3 +48,43 @@ def get_tile_pairs_4_montage(stack, render):
     # Add stack info and reset index
     df_pairs['stack'] = stack
     return df_pairs.reset_index(drop=True)
+
+
+def get_matches_within_section(match_collection, sectionId, render):
+    """Create DataFrame of point matches for a given section
+
+    Parameters
+    ----------
+    match_collection : str
+        Name of match collection
+    sectionId : str
+        Name of section
+        Aka `groupId` in `renderapi` terminology
+    render : `renderapi.render.RenderClient`
+        `render-ws` instance
+
+    Returns
+    -------
+    df_matches : `pd.DataFrame`
+        DataFrame of point matches from a given section
+    """
+    # Initialize point matches DataFrame
+    matches_cols = ['pc', 'pr', 'qc', 'qr', 'N_matches']
+    df_matches = pd.DataFrame(columns=matches_cols)
+
+    # Get point match data as json via `renderapi`
+    matches_json = get_matches_within_group(matchCollection=match_collection,
+                                            groupId=sectionId,
+                                            render=render)
+    # Create DataFrame from json and concatenate with point matches DataFrame
+    df_matches = pd.concat([df_matches, json_normalize(matches_json)],
+                           axis=1, sort=False)
+
+    # Populate DataFrame with row, column and number of matches data
+    df_matches[['pc', 'pr']] = np.stack(df_matches['pId'].apply(lambda x:\
+                                   [int(i) for i in re.findall('\d+', x)[-2:]]))
+    df_matches[['qc', 'qr']] = np.stack(df_matches['qId'].apply(lambda x:\
+                                   [int(i) for i in re.findall('\d+', x)[-2:]]))
+    df_matches['N_matches'] = df_matches['matches.p'].apply(lambda x:\
+                                  np.array(x).shape[1])
+    return df_matches

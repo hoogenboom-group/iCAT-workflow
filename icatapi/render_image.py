900b1c2cc8619e1d7ee4519e8bfb12bcbef3a8bb
lanery <ryanilane@gmail.com>
2020-01-07 09:20:51 +0100

    Update render_image.py
    Former-commit-id: 2370a88a3358f4f555e4e94bb121465798eb5082

--- icatapi/render_image.py
+++ icatapi/render_image.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-from skimage.
+from skimage.io import imread
 
-from renderapi.image import 
+from renderapi.image import get_bb_image
 
 
 def render_tileset_image(stack, z, render):

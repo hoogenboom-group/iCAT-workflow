4fa32f0619425fe496d7c7655aa22dbb00bbb897
lanery <ryanilane@gmail.com>
2020-04-22 11:20:32 +0200

    Accepts renderapi kwargs for tile pair client
    Former-commit-id: 8941d24d81f3b4b75b85835112a944ac05578526

--- icatapi/align.py
+++ icatapi/align.py
@@ -8,8 +8,8 @@ from renderapi.stack import get_z_values_for_stack
 __all__ = ['get_tile_pairs_4_alignment']
 
 
-def get_tile_pairs_4_alignment(stack, render, excludeSameLayerNeighbors=False,
-                               zNeighborDistance=1):
+def get_tile_pairs_4_alignment(stack, render,
+                               **renderapi_kwargs):
     """Collect tile pairs across multiple sections for alignment
 
     Parameters
@@ -34,9 +34,8 @@ def get_tile_pairs_4_alignment(stack, render, excludeSameLayerNeighbors=False,
     tile_pairs_json = tilePairClient(stack=stack,
                                      minz=min(z_values),
                                      maxz=max(z_values),
-                                     excludeSameLayerNeighbors=excludeSameLayerNeighbors,
-                                     zNeighborDistance=zNeighborDistance,
-                                     render=render)
+                                     render=render,
+                                     **renderapi_kwargs)
 
     # Create DataFrame from json
     df_pairs = pd.json_normalize(tile_pairs_json['neighborPairs'])

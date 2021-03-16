c01b510f688e57631330356678bd60be07cf8c52
lanery <ryanilane@gmail.com>
2021-03-16 14:33:19 +0100

    adding function to find overlapping tiles
    Former-commit-id: 90c7b98fc769af335781ed80ba84f1168639c9c9

--- icatapi/correlate.py
+++ icatapi/correlate.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from renderapi.tilespec import get_tile_spec, get_tile_specs_from_minmax_box
+
 
 def bboxes_overlap(bbox_1, bbox_2):
     """Determines if two bounding boxes overlap or coincide
@@ -26,3 +28,44 @@ def bboxes_overlap(bbox_1, bbox_2):
     overlap = ((bbox_1[2] >= bbox_2[0]) and (bbox_1[0] <= bbox_2[2])) and \
               ((bbox_1[3] >= bbox_2[1]) and (bbox_1[1] <= bbox_2[3]))
     return overlap
+
+
+def get_overlapping_tiles(stack, tileId, render, stack2=None):
+    """Finds set of overlapping tiles for a given tile
+
+    Parameters
+    ----------
+    stack : str
+        Stack from which the tile is from
+    tileId : str
+        TileId
+    render : `renderapi.render.RenderClient`
+        `render-ws` instance
+    stack2 : str, optional
+        Stack for which to look for overlapping tiles
+
+    Returns
+    -------
+    overlapping_tileIds : list
+        Tile IDs of overlapping tiles
+    """
+    # Get tile specification
+    ts = get_tile_spec(stack=stack,
+                       tile=tileId,
+                       render=render)
+
+    # Get bounding box of tile specification
+    bbox = ts.bbox
+
+    # Get overlapping tiles
+    overlap_stack = stack2 if stack2 is not None else stack
+    overlapping_tileIds = [ts.tileId for ts in \
+                           get_tile_specs_from_minmax_box(stack=overlap_stack,
+                                                          z=ts.z,
+                                                          xmin=bbox[0],
+                                                          xmax=bbox[2],
+                                                          ymin=bbox[1],
+                                                          ymax=bbox[3],
+                                                          render=render)]
+
+    return overlapping_tileIds

63b3a5bd63843542386c3b0002c7261e538f19e3
lanery <ryanilane@gmail.com>
2021-06-17 13:07:08 +0200

    change `render_image` to `plottting`
    Former-commit-id: f72ec72c07817de26ab1d73ec4286095b2e75fcb

--- icatapi/__init__.py
+++ icatapi/__init__.py
@@ -4,8 +4,8 @@ from .exporto import *
 from .importo import *
 from .montage import *
 from .overlay import *
+from .plotting import *
 
-from .render_image import *
 from .render_pandas import *
 from .render_transforms import *
 
